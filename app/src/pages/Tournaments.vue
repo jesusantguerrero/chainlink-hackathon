077cf36ae0df0d36294d308c1e2f8310903aa6bd
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-06 07:20:17 -0400

    ✨ feat: add tournament list

---
+++ app/src/pages/Tournaments.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Game from '../layouts/Game.vue';
+import TournamentList from '../components/TournamentList.vue';
+</script>
+
+<template>
+    <Game>
+        <div class="px-5 py-3 mt-2 bg-gray-700 current-training">
+            <h2>Admin Pannel</h2>
+        </div>
+        <div class="px-5 py-3 mt-2 bg-gray-700 current-training">
+            <h2 class="md-5">Active Tournaments</h2>
+            <TournamentList />
+        </div>
+    </Game>
+
+</template>
