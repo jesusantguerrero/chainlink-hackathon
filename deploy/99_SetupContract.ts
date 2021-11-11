8806d1b239f8019e2b6cc53cde21e815017f2db5
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-10 21:05:13 -0400

    🔧 chore: deploy different breeds

--- deploy/99_SetupContract.ts
+++ deploy/99_SetupContract.ts
@@ -17,13 +17,19 @@ const premintedTokens = [
   },
   {
     id: 0,
-    breed: 0,
+    breed: 1,
     uri: "https://lh3.googleusercontent.com/bs5rirjgoAV5VQNFwp2EiVurf15o2bbNOIu_sac-nXnP3UTDVh6n2xVCIBLHCwG1odiJ656iP7fzmVPkSEqBYWs1HX73sSsU9FGo=w600",
     claimed: false,
   },
   {
     id: 0,
-    breed: 0,
+    breed: 2,
+    uri: "https://lh3.googleusercontent.com/ioC2jjM0CTXuTCKWHJCsenkO0rmmcWczzlBt25_hbMP_mfaRh5iYAkckwoY1I6tzWM3vNR6RRotyFuOeVk9dUSCWW5GLyK6InmTUNQ=w600",
+    claimed: false,
+  },
+  {
+    id: 0,
+    breed: 3,
     uri: "https://lh3.googleusercontent.com/ioC2jjM0CTXuTCKWHJCsenkO0rmmcWczzlBt25_hbMP_mfaRh5iYAkckwoY1I6tzWM3vNR6RRotyFuOeVk9dUSCWW5GLyK6InmTUNQ=w600",
     claimed: false,
   },
