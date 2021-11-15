577ce6a9f4cfc45860661ff0752c89447cd93784
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-15 08:37:42 -0400

    🎨 feat: update moralis login and header

--- app/src/composables/useMoralis.ts
+++ app/src/composables/useMoralis.ts
@@ -4,6 +4,7 @@ import Web3 from "web3/dist/web3.min";
 import Moralis from "moralis/dist/moralis";
 import { config } from "../config/";
 import { useAppState } from "./useAppState";
+import { AppState } from "./AppState";
 
 const { setUser } = useAppState();
 window.Web3 = Web3;
@@ -13,7 +14,10 @@ Moralis.serverURL = config.moralisServerURL;
 
 export const useMoralis = () => {
   const login = async () => {
-    const user = await Moralis.Web3.authenticate({ provider: window.ethereum });
+    const user = await Moralis.Web3.authenticate({
+      provider: window.ethereum,
+      signingMessage: "Please login to your wallet",
+    });
     if (user) {
       setUser(user);
     }
@@ -23,7 +27,17 @@ export const useMoralis = () => {
     return await Moralis.Web3.logout();
   };
 
+  const init = async () => {
+    if (window.ethereum) {
+      const currentuser = Moralis.User.current();
+      if (currentuser) {
+        AppState.user = currentuser;
+      }
+    }
+  };
+
   return {
+    init,
     login,
     logout,
   };
