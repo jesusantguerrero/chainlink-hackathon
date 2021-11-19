1d584d9e021ac03dba58fed014fdf8fdd5893dbb
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-19 00:03:23 -0400

    🚧 WIP: upload to moralis IPFS

--- tasks/uploadToIPFS.ts
+++ tasks/uploadToIPFS.ts
@@ -1,25 +1,34 @@
+/* eslint-disable node/no-unpublished-import */
+/* eslint-disable node/no-missing-import */
+import * as dotenv from "dotenv";
 import path = require("path");
 import fs = require("fs");
 import Moralis from "../_shared/moralis";
-const basePath = path.dirname(process.execPath);
-const imagesDir = `${basePath}/generated/images`;
+import { useSecureString } from "../_shared/useSecureString";
+
+dotenv.config();
+const { getEnv } = useSecureString(process.env);
+const basePath = getEnv("NFT_ART_PATH");
+const imagesDir = `${basePath}/images`;
 
 // read json data
-const rawData = fs.readFileSync(`${basePath}/generated/json/_metadata.json`);
+const rawData = fs.readFileSync(`${basePath}/json/_metadata.json`);
 const data = JSON.parse(rawData.toString());
 
 const uploadMetadata = async () => {
   for (const edition of data) {
-    const image = fs.createReadStream(
-      path.resolve(imagesDir, `${edition.edition}.png`)
+    const image = fs.readFileSync(
+      path.resolve(imagesDir, `${edition.edition}.png`),
+      { encoding: "base64" }
     );
 
-    const file = new Moralis.File(edition.edition, image.read());
-    await file.saveIPFS();
+    const file = new Moralis.File(edition.edition, { base64: image });
+    const theFile = await file.saveIPFS();
+    console.log(edition.edition, "saved");
 
     const metadata = new Moralis.Object("Metadata");
     metadata.set("edition", edition.edition);
-    metadata.set("image", file);
+    metadata.set("image", theFile);
     await metadata.save();
   }
 };
