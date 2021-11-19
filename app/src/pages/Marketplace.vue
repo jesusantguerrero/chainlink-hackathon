2d09b93f65631a6b0136bbc0f8e754a5a67fff6c
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-18 20:19:28 -0400

    ✨ feat: add marketplace route

---
+++ app/src/pages/Marketplace.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Game from '../layouts/Game.vue';
+import ClaimableList from '../components/ClaimableList.vue';
+</script>
+
+<template>
+    <Game>
+        <div class="mt-2 bg-gray-700 current-training">
+         
+            
+            <div class="px-5 py-3 mt-5">
+                <h2 class="text-2xl font-bold text-gray-300">
+                    Marketplace
+                </h2>
+                <p class="text-gray-300">
+                    Here you can claim, buy and sell items.
+                </p>
+                
+                <ClaimableList class="mt-5" />
+            </div>
+        </div>
+    </Game>
+
+</template>
