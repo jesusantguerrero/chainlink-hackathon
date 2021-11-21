409e12895a465af73bcf5c6a964f9ab255fc0532
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-21 00:55:12 -0400

    ♻️ refactor:  extract RakingRow to its own component

---
+++ app/src/components/TournamentViewRow.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import {  computed, PropType } from "vue";
+import { useContract } from "../composables/useContract"
+import { AtButton } from "atmosphere-ui";
+import { IPlayer } from "../types";
+import { AppState } from "../composables/AppState";
+
+const props = defineProps({
+    currentTokenId: {
+        type: Number,
+    },
+    position: {
+        type: Number,
+    },
+    isJoined: {
+        type: Boolean,
+    },
+    isDarker: {
+        type: Boolean,
+    },
+    player: {
+        type: Object as PropType<IPlayer>,
+        required: true,
+    },
+});
+
+const Tournament = useContract("Tournament", AppState.signer);
+
+const canRequestFight = computed(() => {
+    return props.player.tokenId !== props.currentTokenId && props.isJoined;
+});
+
+const alreadyFought = computed(() => {
+    return props.currentTokenId === props.player.tokenId;
+});
+
+</script>
+
+<template>
+    <tr class="border border-gray-500" :class="{'bg-gray-600': isDarker}">
+        <td class="px-4 py-2 "># {{ position }}</td>
+        <td class="px-4 py-2">
+        <div class="flex">
+            <img :src="player.image" alt="" class="w-20 h-20 rounded-md">
+            <div class="ml-2">
+                <p class="capitalize">{{ player.name }}</p>
+                <AtButton 
+                    class="font-bold bg-purple-400" 
+                    @click="$emit('fight')"
+                    v-if="canRequestFight"
+                    :disabled="alreadyFought"
+                >
+                    Fight {{ player.tokenId }} {{ currentTokenId }}
+                </AtButton>
+            </div>
+        </div>
+        </td>
+        <td class="px-4 py-2">{{ player.record.wins + player.record.losses }}</td>
+        <td class="px-4 py-2">{{ player.record.wins }}</td>
+        <td class="px-4 py-2">{{ player.record.losses }}</td>
+        <td class="px-4 py-2">{{ player.record.draws }}</td>
+        <td class="px-4 py-2">{{ player.points }}</td>
+        <td class="px-4 py-2">{{ player.owner }}</td>
+    </tr>
+</template>
