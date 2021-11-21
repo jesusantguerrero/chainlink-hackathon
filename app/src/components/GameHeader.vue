923185951d0187b37d99ca4de33ae0466a0e3cf4
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-21 18:17:03 -0400

    🚧 WIP: draft landing page final idea

--- app/src/components/GameHeader.vue
+++ app/src/components/GameHeader.vue
@@ -14,7 +14,7 @@ const ProviderState = useWeb3();
                 <i class="fa fa-chevron-left"></i>
             </router-link>
             <h1 class="text-2xl font-bold text-purple-400">
-                Home
+                RoosterFight
             </h1>
         </div>
         <div class="relative flex mr-24 space-x-2">
@@ -22,8 +22,18 @@ const ProviderState = useWeb3();
             <NetworkBadge :chain="ProviderState.chainId" :valid-chain="config.chainId" />
             <WalletAccount :account="ProviderState.account" v-if="ProviderState.account" />
         </div>
-        <div class="absolute top-0 w-20 h-20 overflow-hidden bg-gray-700 border rounded-md right-5">
-            <img :src="AppState.roosters[0].image" v-if="AppState.roosters.length" class="w-full" />
-        </div>
+        <router-link to="/dashboard" class="absolute top-0 w-20 h-20 overflow-hidden bg-gray-700 border rounded-md cursor-pointer right-5" v-if="ProviderState.account">
+            <img :src="AppState.roosters[0].image" v-if="AppState.roosters.length" class="profile-card" />
+        </router-link>
     </div>
 </template>
+
+<style>
+.profile-card {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    min-width: 200px;
+    object-position: -10px -10px;
+}
+</style>
--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -8,18 +8,65 @@ const { state } = useAppState();
 </script>
 
 <template>
-<LandingHeader @connect="$emit('connect')" />
-<div class="justify-center py-20 text-center text-white bg-purple-700">
-    <div>
-        <h1 class="text-2xl font-bold">Train your Roosters and fight</h1>
-        <p class="font-bold"> Build your legacy as a rooster trainer and scale up the tops</p>
+<div :style="{background: 'url(/landing-background.jpg)', backgroundSize: 'cover', backgroundPositionY: 'center'}">
+    <div class="flex justify-center w-full h-10 font-bold text-white bg-purple-400">
+        <div class="flex items-center w-full max-w-7xl">
+            The chainlink hackathon
+        </div>
     </div>
-    <div class="py-12 text-center">
-        <AtButton class="font-bold text-white bg-roti-500" v-if="!state.user" @click="$emit('connect')"> Connect </AtButton>
-       <MintForm v-else />
+    <LandingHeader @connect="$emit('connect')" class="bg-black bg-opacity-60 blur-3xl"/>
+    <div class="justify-center py-20 text-center text-white bg-black bg-opacity-60">
+        <div>
+            <h1 class="text-2xl font-bold">Train your Roosters and fight</h1>
+            <p class="font-bold"> Build your legacy as a rooster trainer and scale up the tops</p>
+        </div>
+        <div class="py-12 text-center">
+            <AtButton class="font-bold text-white bg-purple-400" v-if="!state.user" @click="$emit('connect')"> Connect </AtButton>
+        <MintForm v-else />
+        </div>
+    </div>
+</div>
+<div class="py-20 text-white bg-gray-900">
+    <div class="relative flex justify-center max-w-6xl px-5 py-5 mx-auto bg-gray-800 rounded-lg">
+        <div class="absolute text-3xl font-bold -top-6">
+            Breeds
+        </div>
+        Black Colorao Pinto White
+    </div>
+</div>
+<div class="py-20 text-white bg-gray-900">
+    <div class="relative flex justify-center max-w-6xl px-5 py-5 mx-auto bg-gray-800 rounded-lg">
+        <div class="absolute text-3xl font-bold -top-6">
+            Tournaments
+        </div>
+        Black Colorao Pinto White
+    </div>
+</div>
+<div class="py-20 text-white bg-gray-900">
+    <div class="relative flex flex-col items-center justify-center max-w-6xl px-5 py-5 mx-auto bg-gray-800 rounded-lg">
+        <div class="absolute text-3xl font-bold -top-6">
+            Whitepaper
+        </div>
+        <p>
+            Every rooster have a set of attributes that determine their success in a fight.
+        </p>
+        <p class="mt-5">
+            Strength:
+        </p>
+        <p class="mt-5">
+            Speed:
+        </p>
+        <p class="mt-5">
+            Agility:
+        </p>
     </div>
 </div>
 <div class="py-20 text-white bg-gray-900" id="claim">
-    <ClaimableList class="mx-auto max-w-7xl" />
+    <div class="relative flex justify-center max-w-6xl px-5 py-5 mx-auto bg-gray-800 rounded-lg">
+        <div class="absolute text-3xl font-bold -top-6">
+            Free Roosters to Claim
+        </div>
+        <ClaimableList class="w-full px-5" />
+    </div>
 </div>
 </template>
