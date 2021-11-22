51ad056509bfea317e7263c86a797507ec32b51f
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-21 22:13:02 -0400

    ✨ feat: update landing page

---
+++ app/src/layouts/LandingSection.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    default: "",
+  },
+  subtitle: {
+    type: String,
+    default: "",
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  innerClass: {
+    type: String,
+    default: "bg-gray-700",
+  }
+});
+</script>
+
+<template>
+<div class="py-20 text-white bg-gray-900">
+    <div class="relative flex flex-col items-center justify-center max-w-6xl px-5 py-5 mx-auto rounded-lg" :class="innerClass">
+        <div class="absolute text-3xl font-bold -top-6">
+            {{ title }}
+        </div>
+        <p class="max-w-2xl text-center">
+            <slot name="description">
+             {{ description}}
+            </slot>
+        </p>
+        <div class="mt-10">
+            <slot />
+        </div>
+    </div>
+</div>
+</template>
--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -4,7 +4,10 @@ import { useAppState } from "../composables/useAppState";
 import ClaimableList from "../components/ClaimableList.vue";
 import MintForm from "../components/MintForm.vue";
 import LandingHeader from "../layouts/LandingHeader.vue";
+import LandingSection from "../layouts/LandingSection.vue";
 const { state } = useAppState();
+
+const breeds = ["black", "colorao", "pinto", "white"]; 
 </script>
 
 <template>
@@ -26,41 +29,53 @@ const { state } = useAppState();
         </div>
     </div>
 </div>
-<div class="py-20 text-white bg-gray-900">
-    <div class="relative flex justify-center max-w-6xl px-5 py-5 mx-auto bg-gray-800 rounded-lg">
-        <div class="absolute text-3xl font-bold -top-6">
-            Breeds
+<LandingSection title="Breeds" >
+        <template #description>
+            The breed is determined by the color of the chest of the Rooster the other combinations are stylistic
+            but the breed add attributes on mint that make Roosters stronger. but with training and fighting  you can update the natural abilities of your Rooster
+        </template>
+        
+        <div class="flex">
+            <div class="text-center" v-for="breed in breeds">
+                <img :src="`/images/breed_${breed}.png`" />
+                <span class="text-xl font-bold text-center capitalize">{{ breed }}</span>
+            </div>
         </div>
-        Black Colorao Pinto White
-    </div>
-</div>
-<div class="py-20 text-white bg-gray-900">
-    <div class="relative flex justify-center max-w-6xl px-5 py-5 mx-auto bg-gray-800 rounded-lg">
-        <div class="absolute text-3xl font-bold -top-6">
-            Tournaments
-        </div>
-        Black Colorao Pinto White
-    </div>
-</div>
-<div class="py-20 text-white bg-gray-900">
-    <div class="relative flex flex-col items-center justify-center max-w-6xl px-5 py-5 mx-auto bg-gray-800 rounded-lg">
-        <div class="absolute text-3xl font-bold -top-6">
-            Whitepaper
-        </div>
-        <p>
+</LandingSection>
+<LandingSection class="bg-purple-500" title="Tournaments" inner-class="bg-purple-700">
+         <template #description>
+            Tournaments are periodic events where the Roosters can join by paying a determined fee
+            with a duration of a week were you can fight once with every other Rooster in the tournament if you win you got 3 points at the end the rooster with more points get the prize.
+         </template>
+        
+        <img src="/images/tournaments.png" alt="tournaments" class="rounded-lg">
+</LandingSection>
+<LandingSection title="Fighting System">
+        <template #description>
             Every rooster have a set of attributes that determine their success in a fight.
-        </p>
-        <p class="mt-5">
-            Strength:
-        </p>
-        <p class="mt-5">
-            Speed:
-        </p>
-        <p class="mt-5">
-            Agility:
-        </p>
-    </div>
-</div>
+
+            In a fight both Roosters have an opportunity to throw a punch and the damage will be between min-damage (strength) and max-damage (strength + speed).
+            The rooster that cause more damage is the winner.
+        </template>
+        <div>
+            <p class="mt-5">
+                Strength:
+            </p>
+            <p class="mt-5">
+                Speed:
+            </p>
+            <p class="mt-5">
+                Agility:
+            </p>
+        </div>
+</LandingSection>
+<LandingSection class="bg-roti-500" title="Train and Upgrade" inner-class="bg-roti-700">
+         <template #description>
+            After each fight a rooster will gain attribute points that you can assign to whatever attribute you like.
+         </template>
+        
+        <img src="/images/upgrade.png" alt="upgrade" class="rounded-lg">
+</LandingSection>
 <div class="py-20 text-white bg-gray-900" id="claim">
     <div class="relative flex justify-center max-w-6xl px-5 py-5 mx-auto bg-gray-800 rounded-lg">
         <div class="absolute text-3xl font-bold -top-6">
