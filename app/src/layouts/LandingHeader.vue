ecfed1da702fe8b076326cd5d10b3fcea9d0b995
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-26 08:33:43 -0400

    💄 feat: improve landing and change primary color

--- app/src/layouts/LandingHeader.vue
+++ app/src/layouts/LandingHeader.vue
@@ -17,11 +17,11 @@ const links = [{
 const actions = [{
     name: 'Learn more',
     url: '/login',
-    class: 'text-purple-300'
+    class: 'text-primary-500'
 }, {
     name: 'Connect',
     emit: true,
-    class: "relative px-5 py-2 overflow-hidden rounded-md focus:outline-none hover:bg-opacity-80 px-10 font-bold text-white bg-purple-400"
+    class: "relative px-5 py-2 overflow-hidden rounded-md focus:outline-none hover:bg-primary-700 px-10 font-bold text-white bg-primary"
 }]
 </script>
 
@@ -34,6 +34,9 @@ const actions = [{
         dot-class="text-transparent" 
         @action="$emit('connect')" 
     >
+        <template #brand>
+            <img src="../assets/logo.svg" class="w-40 mt-5"/>
+        </template>
         <template #actions v-if="state.user">
             <div class="text-white">
             {{ state.user?.getUsername() }}
